b13395f6b397b5de8885bb82db130c5e8f8651ba
Adrien Girault <77406031+Adriensud@users.noreply.github.com>
2023-11-09 15:45:16 +0100

    mise à jour de la BDD

--- rodelika/PurpleDragon.sql
+++ rodelika/PurpleDragon.sql
@@ -8,10 +8,10 @@ etu_prenom VARCHAR(255),
 PRIMARY KEY(etu_num)
 );
 INSERT INTO etudiant (etu_num, etu_nom, etu_prenom) VALUES
-(1, 'Thompson', 'Allan'),
-(2, 'Castafiore', 'Bianca'),
-(3, 'Lampion', 'Séraphin'),
-(4, 'Da Figueira', 'Oliveira');
+(1, 'Arvin-Berod', 'Maxence'),
+(2, 'Hubert', 'Quentin'),
+(3, 'El Beki', 'Sohayb'),
+(4, 'Girault', 'Adrien');
 CREATE TABLE type(
 type_operation VARCHAR(255),
 PRIMARY KEY(type_operation)
@@ -33,11 +33,11 @@ INSERT INTO compte (etu_num, opr_date, opr_montant, opr_libelle, type_operation)
 (1, '2023-05-15 15:18:44', '-0.20', 'Cafe', 'Dépense'),
 (1, '2023-07-11 11:14:05', '-0.20', 'Chocolat', 'Dépense'),
 (1, '2023-07-24 15:20:20', '-0.20', 'Café', 'Dépense'),
-(2, '2023-05-09 11:18:32', '1.00', 'Atelier Avec Dupont', 'Bonus'),
+(2, '2023-05-09 11:18:32', '1.00', 'Blague marrante durant un cours', 'Bonus'),
 (2, '2023-05-16 08:23:32', '1.00', 'Inital', 'Bonus'),
-(2, '2023-07-04 15:24:35', '1.00', 'Projet avec Pr Tournesol', 'Bonus'),
+(2, '2023-07-04 15:24:35', '1.00', 'bonne initiative', 'Bonus'),
 (2, '2023-07-06 15:24:35', '-0.20', 'Thé', 'Dépense'),
-(2, '2023-07-26 15:23:32', '1.00', 'Chanson opéra pour Haddock', 'Bonus'),
+(2, '2023-07-26 15:23:32', '1.00', 'Belle présentation', 'Bonus'),
 (3, '2023-05-18 15:26:38', '1.00', 'initial', 'Bonus'),
 (4, '2023-07-13 15:27:04', '1.00', 'intiial', 'Bonus'),
 (4, '2023-07-21 13:10:40', '-0.20', 'Café', 'Dépense');
