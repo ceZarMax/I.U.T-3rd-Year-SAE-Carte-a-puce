a4792c0dc30359d64547058a9aa5aa39b73fd21e
Adriensud <adri1girault@gmail.com>
2023-11-16 18:38:12 +0100

    Berlicum test 1.0

--- Berlicum.py
+++ Berlicum.py
@@ -1,4 +1,7 @@
 import mysql.connector
+import smartcard.System as scardsys
+import smartcard.util as scardutil
+import smartcard.Exceptions as scardexcp
 
 # Assurez-vous de remplacer les valeurs de connexion par les vôtres
 cnx = mysql.connector.connect(user='root',
@@ -53,20 +56,53 @@ def transferer_bonus(etu_num):
     else:
         print("Aucun bonus à transférer.")
 
-def consulter_credit(etu_num):
-    sql = "SELECT etu_num, SUM(opr_montant) as credit FROM compte WHERE etu_num = %s GROUP BY etu_num;"
-    val = (etu_num,)
-    cursor = cnx.cursor()
-    cursor.execute(sql, val)
-    credit = cursor.fetchone()
-    if credit:
-        print(f"Crédit disponible: {credit[1]}")
+def consulter_credit():
+    # Ici, nous devons construire et envoyer une commande APDU à la carte pour lire le solde
+    # L'instruction pour lire le solde est '0x01' dans la classe '0x82'
+    apdu = [0x82, 0x01, 0x00, 0x00, 0x02]  # '0x02' est la longueur des données attendues (le solde est un uint16_t)
+
+    try:
+        # Envoi de la commande APDU à la carte et réception de la réponse
+        data, sw1, sw2 = conn_reader.transmit(apdu)
+        if sw1 == 0x90 and sw2 == 0x00:  # Vérification du statut de réponse
+            solde = int.from_bytes(data, byteorder='big')  # Convertir les données en nombre
+            print(f"Solde actuel sur la carte: {solde} centimes")
+        else:
+            print(f"Erreur lors de la lecture du solde de la carte: SW1 = {sw1}, SW2 = {sw2}")
+    except Exception as e:
+        print(f"Erreur lors de la communication avec la carte: {e}")
+
     else:
         print("Aucune information de crédit disponible.")
 
-def recharger_credit():
-    # Puisque le processus de recharge est fictif, nous simulons simplement une transaction réussie.
-    print("Votre carte a été rechargée avec succès.")
+def lire_solde_db(etu_num):
+    sql = "SELECT SUM(opr_montant) as total_bonus FROM Compte WHERE etu_num = %s AND type_operation = 'Bonus';"
+    val = (etu_num,)
+    cursor = cnx.cursor()
+    cursor.execute(sql, val)
+    result = cursor.fetchone()
+    return result[0] if result else 0
+
+def recharger_carte(etu_num):
+    # Lire le solde de la base de données
+    solde_db = lire_solde_db(etu_num)  # Implémentez cette fonction pour lire le solde de la base de données
+
+    # Convertir le solde en format approprié pour la carte (par exemple, en bytes)
+    solde_bytes = solde_db.to_bytes(2, byteorder='big')  # Supposons que le solde est un uint16_t
+
+    # Construire la commande APDU pour mettre à jour le solde sur la carte
+    # Supposons que l'instruction pour écrire le solde est '0x02' dans la classe '0x82'
+    apdu = [0x82, 0x02, 0x00, 0x00] + list(solde_bytes)
+
+    try:
+        # Envoi de la commande APDU à la carte et réception de la réponse
+        sw1, sw2 = conn_reader.transmit(apdu)
+        if sw1 == 0x90 and sw2 == 0x00:  # Vérification du statut de réponse
+            print(f"Solde de la carte mis à jour: {solde_db} centimes")
+        else:
+            print(f"Erreur lors de la mise à jour du solde de la carte: SW1 = {sw1}, SW2 = {sw2}")
+    except Exception as e:
+        print(f"Erreur lors de la communication avec la carte: {e}")
 
 def main():
     print_hello_message()
